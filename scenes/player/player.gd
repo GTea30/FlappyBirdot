c4a7ca269ff87873eff062ec01436c22a11a636a
GTea <grace.tveit@gmail.com>
2025-05-01 14:22:43 -0700

    Setting Gravity

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D;
 
 @export var flap_velocity: float = 200;
+@export var gravity = 10;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +10,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+    # Fall
+    self.velocity.y += gravity;
+    move_and_slide();
     pass
