0677e59f0a5bf90793a7fd03bb4ffb9e6430f276
GTea <grace.tveit@gmail.com>
2025-05-01 14:29:47 -0700

    Setting Bird to rotate when falling and flapping

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D;
 
 @export var flap_velocity: float = 200;
 @export var gravity = 10;
+@export var tilt: float = 0.05;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,10 +13,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
     # Fall
     self.velocity.y += gravity;
+    if self.rotation < PI / 2 and self.velocity.y >= 0:
+        self.rotation += tilt;
 
     # Flap
     if Input.is_action_just_pressed("flap"):
         self.velocity.y = flap_velocity * -1;
+        self.rotation = PI / -5;
 
     # Set Position
     move_and_slide();
